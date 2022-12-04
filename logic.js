8da72e29c56e3f97b5aadf0d3d3fc1b6ae92869c
Nesquiker <seanwreid11@gmail.com>
2022-12-04 11:43:28 -0500

    Added some transitions and implemented AI

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@ CURRENT_GAME_BOARD = [];
 let HUMAN;
 let COMPUTER;
 let MARK_COUNT;
+let ACTIVE_PLAYER;
 
 function build2DArray() {
     let output = [0, 0, 0];
@@ -18,40 +19,112 @@ class competitor {
     }
 }
 
+function transition(grid) {
+    grid.textContent = ACTIVE_PLAYER.marker;
+    grid.classList.remove("fade");
+}
+
 function humanMarkBoard(e) {
     // Tried a combined board marking approach early on but found that accessing via
     // the button event for a person and through a separate function was incompatible.
+    if (ACTIVE_PLAYER === HUMAN) {
+        return;
+    }
     if (e.target.textContent !== "") {
         return 0;
     }
-    const grid = document.querySelector("#G3");
-    console.log((grid.id));
     MARK_COUNT++;
-    e.target.textContent = HUMAN.marker;
+    ACTIVE_PLAYER = HUMAN;
     let grid_num = Number(e.target.id[1]);
     let row = Math.floor(grid_num / 3);
     let column = grid_num % 3;
     CURRENT_GAME_BOARD[row][column] = HUMAN.marker;
+    transition(e.target);
     if (checkBoard()) {
-        computerTurn();
+        setTimeout(() => {computerTurn();}, 300);
     }
 }
 
 function computerTurn() {
     MARK_COUNT++;
-    let nomarked = false;
-    while (!nomarked) {
-        let try_mark = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)];
-        let curr = CURRENT_GAME_BOARD[try_mark[0]][try_mark[1]];
-        if (curr === 0) {
-            CURRENT_GAME_BOARD[try_mark[0]][try_mark[1]] = COMPUTER.marker;
-            let grid_num = try_mark[0] * 3 + try_mark[1];
-            const grid = document.querySelector("#G" + grid_num);
-            grid.textContent = COMPUTER.marker;
-            nomarked = true;
+
+    ACTIVE_PLAYER = COMPUTER;
+    let next_play = computerLogic();
+    CURRENT_GAME_BOARD[next_play[0]][next_play[1]] = COMPUTER.marker;
+    let grid_num = next_play[0] * 3 + next_play[1];
+    const grid = document.querySelector("#G" + grid_num);
+    transition(grid);
+    checkBoard();
+}
+
+function computerLogic() {
+    let cell_danger_1 = build2DArray();
+    let cell_danger_2 = build2DArray();
+    let lose_location = null;
+    let free_entry_pos = null;
+    for (let line of lineGenerator()) {
+        let line_o = 0;
+        let line_x = 0;
+        let free_entries = [];
+        for (let entry of line) {
+            let val = CURRENT_GAME_BOARD[entry[0]][entry[1]];
+            if (val === "X") {
+                line_x++;
+            } else if (val === "O") {
+                line_o++;
+            } else {
+                free_entries.push(entry);
+                if (free_entry_pos === null) {
+                    free_entry_pos = entry;
+                }
+            }
+        }
+        let comp = 0;
+        let human = 0;
+        if (line_o === 0 || line_x === 0) {
+            if (COMPUTER.marker === 'X') {
+                comp = line_x;
+                human = line_o;
+            } else {
+                comp = line_o;
+                human = line_x;
+            }
+            if (comp === 2) {
+                return free_entries[0];
+            } else if (human === 2) {
+                lose_location = free_entries[0];
+            }else if (human === 1) {
+                for (let entry of free_entries) {
+                    cell_danger_1[entry[0]][entry[1]]++;
+                }
+            } else if (human ===0 && comp === 0) {
+                for (let entry of free_entries) {
+                    cell_danger_2[entry[0]][entry[1]]++;
+                }
+            }
+        }
+
+    }
+    if (lose_location) {
+        return lose_location;
+    }
+    let curr_max = [0, 0];
+    let curr_max_loc = [-1, -1];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let curr_loc = [i, j];
+            let d_1 = cell_danger_1[i][j];
+            let d_2 = cell_danger_2[i][j];
+            if (d_1 > curr_max[0] || (d_1 === curr_max[0] && d_2 > curr_max[1])) {
+                curr_max_loc = curr_loc;
+                curr_max = [d_1, d_2];
+            }
         }
     }
-    checkBoard();
+    if (curr_max_loc[0] === -1) {
+        return free_entry_pos;
+    }
+    return curr_max_loc;
 }
 
 function checkBoard() {
@@ -81,6 +154,7 @@ function checkBoard() {
 function makeResultPage(result) {
     // Reset Board to start game menu
     const board = document.querySelector(".board_container");
+    board.classList.add("fade_board");
     const result_page = document.createElement("div");
     result_page.classList.add("start_menu");
     const title = document.createElement("h1");
@@ -95,7 +169,8 @@ function makeResultPage(result) {
     button_container.appendChild(button_player);
     result_page.appendChild(title);
     result_page.appendChild(button_container);
-    board.replaceChildren(result_page);
+    setTimeout(() => {board.replaceChildren(result_page);}, 1000);
+    setTimeout(() => {board.classList.remove("fade_board");}, 1500);
 }
 
 
@@ -146,7 +221,8 @@ function makeBoard(e) {
     const board_size = 9;
     for (let i = 0; i < board_size; i++) {
         const grid = document.createElement("div");
-        grid.classList.add("play_grid")
+        grid.classList.add("play_grid");
+        grid.classList.add("fade");
         grid.id = "G" +  i;
         grid.addEventListener("click", humanMarkBoard);
         game_board.appendChild(grid);
